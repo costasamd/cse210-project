45b85c1b1630b4fdff41a8c96eaef422c187be5c
costasamd <costasamd@icloud.com>
2025-05-13 23:49:26 +0100

    week 2 lesson task

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2024;
+        job1._endYear = 2030;
+
+        Job job2 = new();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Google";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+        Resume resume1 = new();
+        resume1._name = "Samuel D.C";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResume();
+
+
+
     }
 }
